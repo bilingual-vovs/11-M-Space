67e10149d5fdc2dce0c5ad849bcfad267c78ea4a
bilingual-vovs <vovavovsart@gmail.com>
2023-01-02 11:22:41 +0200

    init

--- src/Components/Member/MemberFunc.jsx
+++
@@ -1,9 +0,0 @@
-import { useParams } from "react-router"
-import React from "react"
-import Member from "./Member"
-
-
-export default function MemberFunc () {
-    const id = useParams().id
-    return <Member id={id}/>
-}
--- src/Settings.js
+++
@@ -1,3 +0,0 @@
-export const apiUrl = "https://Server.bilingualvovs.repl.co"
-export const imgPath = (id)=> "/image/"+id+".png"
-export const memberPath = "/members/"
